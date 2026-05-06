a10a5291c3fff5b1eec0860aa7b9003b3bc5ea4b
Bhavyashree - Bangalore - Software Engineer <BhavyashreeTP@wepsol.com>
2026-05-06 16:24:12 +0530

    Initial commit - mini zomato django project

--- backend/database.py
+++ backend/database.py
@@ -2,10 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://expense_db_aovt_user:WchyxnySanXwgStsGmDvI8TG6l0l4eC3@dpg-d7so2ihkh4rs73980ha0-a.singapore-postgres.render.com/expense_db_aovt"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 

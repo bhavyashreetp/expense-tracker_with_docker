63a795ae02feccff64034c5d6dc2ff737e097d47
Bhavyashree - Bangalore - Software Engineer <BhavyashreeTP@wepsol.com>
2026-05-08 12:40:34 +0530

    updated API URL and fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,18 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "https://expense-tracker-with-docker-1.onrender.com"],
+    
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://127.0.0.1:3000",
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    
+    
 )
 
 def get_db():

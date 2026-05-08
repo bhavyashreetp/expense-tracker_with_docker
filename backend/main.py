edd00c32a507c79da7eea421b5cd8948016c19ec
Bhavyashree - Bangalore - Software Engineer <BhavyashreeTP@wepsol.com>
2026-05-08 14:10:03 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -12,10 +12,9 @@ app.add_middleware(
     CORSMiddleware,
     
     allow_origins=[
+        "https://expense-tracker-with-docker-1.onrender.com",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "http://127.0.0.1:3000",
-        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
